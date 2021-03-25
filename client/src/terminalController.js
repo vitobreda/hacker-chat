e48b3075ccb09b1a7a95e9e2a588d0f326722ff8
Vito Breda <vito_odoni@hotmail.com>
2021-03-24 22:07:23 -0300

    first commit

---
+++ client/src/terminalController.js
@@ -0,0 +1,117 @@
+import ComponentBuilder from "./components.js";
+import { constants } from "./constants.js";
+
+export default class TerminalController {
+  #usersCollor = new Map();
+
+  constructor() {}
+
+  #pickCollor() {
+    return `#` + (((1 << 24) * Math.random()) | 0).toString(16) + `-fg`;
+  }
+
+  #getUserCollor(userName) {
+    if (this.#usersCollor.has(userName)) return this.#usersCollor.get(userName);
+
+    const collor = this.#pickCollor();
+    this.#usersCollor.set(userName, collor);
+
+    return collor;
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+      console.log(message);
+      this.clearValue();
+    };
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return (msg) => {
+      const { userName, message } = msg;
+      const collor = this.#getUserCollor(userName);
+      chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`);
+      screen.render();
+    };
+  }
+
+  #onLogChanged({ screen, activityLog }) {
+    return (msg) => {
+      const [userName] = msg.split(/\s/);
+      const collor = this.#getUserCollor(userName);
+
+      activityLog.addItem(`{${collor}}{bold}${msg.toString()}{/}`);
+      screen.render();
+    };
+  }
+
+  #onStatusChanged({ screen, status }) {
+    return (users) => {
+      //get first item from status.items
+      const { content } = status.items.shift();
+
+      status.clearItems();
+
+      //add first items on clear array
+      status.addItem(content);
+
+      users.forEach((userName) => {
+        const collor = this.#getUserCollor(userName);
+        status.addItem(`{${collor}}{bold}${userName}{/}`);
+      });
+
+      screen.render();
+    };
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on(
+      constants.events.app.MESSAGE_RECEIVED,
+      this.#onMessageReceived(components)
+    );
+    eventEmitter.on(
+      constants.events.app.ACTIVITYLOG_UPDATED,
+      this.#onLogChanged(components)
+    );
+    eventEmitter.on(
+      constants.events.app.STATUS_UPDATED,
+      this.#onStatusChanged(components)
+    );
+  }
+
+  async initializeTable(eventEmitter) {
+    const components = new ComponentBuilder()
+      .setScreen({ title: "HackerChat" })
+      .setLayoutComponent()
+      .setChatComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setActivityLogComponent()
+      .setStatusComponent()
+      .build();
+
+    this.#registerEvents(eventEmitter, components);
+    components.input.focus();
+    components.screen.render();
+
+    //only for test
+    setInterval(() => {
+      const users = [];
+      eventEmitter.emit("message:received", {
+        message: "hey",
+        userName: "vito",
+      });
+      eventEmitter.emit("message:received", {
+        message: "teste",
+        userName: "bia",
+      });
+      eventEmitter.emit("activityLog:updated", "vito join");
+      eventEmitter.emit("activityLog:updated", "bia join");
+      eventEmitter.emit("activityLog:updated", "bia left");
+      eventEmitter.emit("activityLog:updated", "vito left");
+
+      users.push("vito", "bia");
+      eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+    }, 2000);
+  }
+}
